23c6211205c7364c3599dd6013371f6cbb5e3b4a
Cleiton Alves <cleiton_jc@yahoo.com.br>
2026-02-21 20:14:01 -0300

    implementa função de predição e ajusta normalização de dados para entrada do modelo

--- modulo-02/exemplo-00/index.js
+++ modulo-02/exemplo-00/index.js
@@ -53,15 +53,26 @@ async function trainModel(inputXs, outputYs) {
             verbose: 0,
             epochs: 200,
             shuffle: true,
-            callbacks: {
-                onEpochEnd: (epoch, log) => console.log(
-                    `Epoch: ${epoch}: loss = ${log.loss}`
-                )
-            }
+            // callbacks: {
+            //     onEpochEnd: (epoch, log) => console.log(
+            //         `Epoch: ${epoch}: loss = ${log.loss}`
+            //     )
+            // }
         }
     )
 
     return model
+}
+
+async function predict(model, pessoa) {
+    // transformar o array js para o tensor (tfjs)
+    const tfInput = tf.tensor2d(pessoa)
+
+    // Faz a predição (output será um vetor de 3 probabilidades)
+    const pred = model.predict(tfInput)
+    const predArray = await pred.array()
+    
+    return predArray[0].map((prob, index) => ({ prob, index }))
 }
 
 // Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
@@ -77,16 +88,16 @@ async function trainModel(inputXs, outputYs) {
 //     [0.33, 1, 0, 0, 1, 0, 0], // Erick
 //     [0, 0, 1, 0, 0, 1, 0],    // Ana
 //     [1, 0, 0, 1, 0, 0, 1]     // Carlos
-//     [0.67, 0, 0, 1, 0, 0, 1]  // Cleiton
+//     [0.67, 0, 0, 1, 1, 0, 0]  // Cleiton
 // ]
 
 // Usamos apenas os dados numéricos, como a rede neural só entende números.
 // tensorPessoasNormalizado corresponde ao dataset de entrada do modelo.
-const tensorPessoasNormalizado = [
+const tensorPessoaNormalizado = [
     [0.33, 1, 0, 0, 1, 0, 0], // Erick
     [0, 0, 1, 0, 0, 1, 0],    // Ana
     [1, 0, 0, 1, 0, 0, 1],     // Carlos
-    [0.67, 0, 0, 1, 0, 0, 1]   // Cleiton
+    [0.67, 0, 0, 1, 1, 0, 0]   // Cleiton
 ]
 
 // Labels das categorias a serem previstas (one-hot encoded)
@@ -100,9 +111,34 @@ const tensorLabels = [
 ];
 
 // Criamos tensores de entrada (xs) e saída (ys) para treinar o modelo
-const inputXs = tf.tensor2d(tensorPessoasNormalizado)
+const inputXs = tf.tensor2d(tensorPessoaNormalizado)
 const outputYs = tf.tensor2d(tensorLabels)
 
 // quanto mais dados, melhor!
 // assim o algoritmo consegue entender melhor os padrões complexos dos dados
-const model = trainModel(inputXs, outputYs)
+const model = await trainModel(inputXs, outputYs)
+
+const pessoa = { name: 'Zé', cor: 'verde', localizacao: 'Curitiba', idade: 28 }
+// Normalizando a idade da nova pessoal usando o mesmo padrão do treino
+// Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25) = 0.2
+
+const pessoaTensorNormalizado = [
+    [
+        0.2, // idade normalizada
+        1,   // cor azul
+        0,   // cor vermelho
+        0,   // cor verde
+        0,   // localização São Paulo
+        1,   // localização Rio
+        0    // localização Curitiba
+    ]
+]
+
+const predictions = await predict(model, pessoaTensorNormalizado)
+const results = predictions
+    .sort((a, b) => b.prob - a.prob)
+    .map(p => `${labelsNomes[p.index]} (${(p.prob * 100).toFixed(2)}%)`)
+    .join('\n')
+
+console.log('results')
+console.log(results)
