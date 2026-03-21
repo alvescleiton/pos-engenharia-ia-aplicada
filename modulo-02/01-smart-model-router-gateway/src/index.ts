d23fff4f9402ce8fc34361ad56e79a6fad217b0b
Cleiton Alves <cleiton_jc@yahoo.com.br>
2026-03-20 22:29:53 -0300

    feat: add initial project structure with Fastify server and TypeScript configuration

---
+++ modulo-02/01-smart-model-router-gateway/src/index.ts
@@ -0,0 +1,17 @@
+import { createServer } from './server.ts'
+
+const app = createServer()
+
+await app.listen({ port: 3000, host: '0.0.0.0' })
+console.log('Server runing at 3000')
+
+app.inject({
+    method: 'POST',
+    url: '/chat',
+    body: {
+        question: 'Hello World!'
+    }
+}).then((response) => {
+    console.log('Response status: ', response.statusCode)
+    console.log('Response body: ', response.body)
+})
